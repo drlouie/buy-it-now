38d5b32e18f7c605086fee05767f5c781d45b7eb
drlouie <drlouie@louierd.com>
2017-11-02 01:24:42 -0700

    Update wp-buyitnow.php

--- wordpress-plugin-buy-it-now/wp-buyitnow.php
+++ wordpress-plugin-buy-it-now/wp-buyitnow.php
@@ -1,5 +1,5 @@
 <?php
-if(!function_exists('add_action')){header('HTTP/1.1 403 Forbidden');header('Location: http://www.domainwebsite.com/');exit();}
+if(!function_exists('add_action')){header('HTTP/1.1 403 Forbidden');header('Location: /');exit();}
 /**
 * @package Buy It Now, WordPress
 */
